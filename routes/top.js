48167d11a061ac32745f68b781e1efd8f2bdd2a4
MikiKentaro <ken6131xperia@gmail.com>
2015-05-20 23:14:44 +0900

    0520mobileTest

--- routes/top.js
+++ routes/top.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
+router.get('/', function(req, res, next) {
+
+
+//exports.index = function(req, res){
+
+userAgent = req.headers['user-agent'].toLowerCase();
+
+
+
+    // 表示するページ出し分け
+    if(userAgent.indexOf("android") != -1
+             || userAgent.indexOf("iphone") != -1
+             || userAgent.indexOf("ipod") != -1){
+        res.render('top_m', {title: 'top mobile'});
+    }
+    else{
+        res.render('top', {title:'top pc'});
+    }
+
+//}
+
+
+});
+
+
+
+
+
+
+
+
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
   res.render('top', { title: 'top' });
 });
-
+*/
 module.exports = router;
