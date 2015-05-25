0bcfe6b726da6d1580c002f1e39973e050d2843f
MikiKentaro <ken6131xperia@gmail.com>
2015-05-25 23:07:36 +0900

    0525 camera6

--- public/javascripts/cameraSam.js
+++ public/javascripts/cameraSam.js
@@ -67,8 +67,8 @@
     var cCtx = cEle.getContext('2d');
     var vEle = document.getElementById('myVideo');
 
-    cEle.width  = 300px;   // canvasの幅と高さを、動画の幅と高さに合わせる
-    cEle.height = 300px;
+    cEle.width  = 300;   // canvasの幅と高さを、動画の幅と高さに合わせる
+    cEle.height = 300;
 
     cCtx.drawImage(vEle, 0, 0);  // canvasに関数実行時の動画のフレームを描画
 }
