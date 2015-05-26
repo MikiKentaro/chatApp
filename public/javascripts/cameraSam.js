90e3cfbe1b0c97762c8ff8f221ab75074415ac00
MikiKentaro <ken6131xperia@gmail.com>
2015-05-26 22:29:07 +0900

    0526 kk

--- public/javascripts/cameraSam.js
+++ public/javascripts/cameraSam.js
@@ -97,8 +97,8 @@ var aspect=vEle.videoWidth/vEle.videoHeight;
 	alert(vEle.videoWidth+"aa"+vEle.videoHeight)
 	var tate=300/aspect;
 
-    //cEle.width  = 300;   // canvasの幅と高さを、動画の幅と高さに合わせる
-    //cEle.height = 300;
+    //cEle.width  = vEle.videoWidth;   // canvasの幅と高さを、動画の幅と高さに合わせる
+    //cEle.height = vEle.videoHeight;
     
 	
 	
@@ -106,7 +106,7 @@ var aspect=vEle.videoWidth/vEle.videoHeight;
 	
 	
 
-    cCtx.drawImage(vEle, 0, 0,300,300);  // canvasに関数実行時の動画のフレームを描画
+    cCtx.drawImage(vEle, 0, 0,vEle.videoWidth,vEle.videoHeight);  // canvasに関数実行時の動画のフレームを描画
 	
 	
 	
